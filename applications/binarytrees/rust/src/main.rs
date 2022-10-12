ab144c57746e25fa88a987b30469ce6c65426157
Andrea Marino <am.marinoandrea@gmail.com>
2022-10-12 13:30:44 +0200

    Updated Rust `binarytrees` dependencies and fixed benchmark code

--- applications/binarytrees/rust/src/main.rs
+++ applications/binarytrees/rust/src/main.rs
@@ -1,192 +1,75 @@
 // The Computer Language Benchmarks Game
 // https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
 //
-// contributed by Tom Kaitchuck
+// contributed by the Rust Project Developers
+// contributed by TeXitoi
+// contributed by Cristi Cobzarenco
+// contributed by Matt Brubeck
 
-// Based on k-nucleotide Rust #7
-// Switched to used Hashbrown and removed custom hash code.
-// Removed rayon and use threads directly
-// Copied the read_input function from k-nucleotide Rust #4
+extern crate typed_arena;
+extern crate rayon;
 
-use hashbrown::HashMap;
-use std::io::{BufRead, BufReader};
-use std::sync::Arc;
-use std::thread;
+use typed_arena::Arena;
+use rayon::prelude::*;
 
-type Map = HashMap<Code, u32>;
+struct Tree<'a> {
+    children: Option<(&'a Tree<'a>, &'a Tree<'a>)>,
+}
 
-#[derive(Hash, PartialEq, PartialOrd, Ord, Eq, Clone, Copy)]
-struct Code(u64);
-impl Code {
-    fn push(&mut self, c: u8, mask: u64) {
-        self.0 <<= 2;
-        self.0 |= c as u64;
-        self.0 &= mask;
-    }
-    fn from_str(s: &str) -> Code {
-        let mask = Code::make_mask(s.len());
-        let mut res = Code(0);
-        for c in s.as_bytes() {
-            res.push(Code::encode_byte(c), mask);
-        }
-        res
-    }
-    fn to_string(&self, frame: usize) -> String {
-        let mut res = vec![];
-        let mut code = self.0;
-        for _ in 0..frame {
-            let c = match code as u8 & 0b11 {
-                c if c == Code::encode_byte(&b'A') => b'A',
-                c if c == Code::encode_byte(&b'T') => b'T',
-                c if c == Code::encode_byte(&b'G') => b'G',
-                c if c == Code::encode_byte(&b'C') => b'C',
-                _ => unreachable!(),
-            };
-            res.push(c);
-            code >>= 2;
-        }
-        res.reverse();
-        String::from_utf8(res).unwrap()
-    }
-    fn make_mask(frame: usize) -> u64 {
-        (1u64 << (2 * frame)) - 1
-    }
-    #[inline(always)]
-    fn encode_byte(c: &u8) -> u8 {
-        (c >> 1) & 0b11
+fn item_check(tree: &Tree) -> i32 {
+    if let Some((left, right)) = tree.children {
+        1 + item_check(right) + item_check(left)
+    } else {
+        1
     }
 }
 
-struct Iter<'a> {
-    iter: std::slice::Iter<'a, u8>,
-    code: Code,
-    mask: u64,
-}
-impl<'a> Iter<'a> {
-    fn new(input: &[u8], frame: usize) -> Iter {
-        let mut iter = input.iter();
-        let mut code = Code(0);
-        let mask = Code::make_mask(frame);
-        for c in iter.by_ref().take(frame - 1) {
-            code.push(*c, mask);
-        }
-        Iter {
-            iter: iter,
-            code: code,
-            mask: mask,
-        }
+fn bottom_up_tree<'r>(arena: &'r Arena<Tree<'r>>, depth: i32)
+                  -> &'r Tree<'r> {
+    let mut tree = arena.alloc(Tree { children: None });
+    if depth > 0 {
+        let right = bottom_up_tree(arena, depth - 1);
+        let left = bottom_up_tree(arena, depth - 1);
+        tree.children = Some((left, right))
     }
-}
-impl<'a> Iterator for Iter<'a> {
-    type Item = Code;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(|&c| {
-            self.code.push(c, self.mask);
-            self.code
-        })
-    }
+    tree
 }
 
-fn gen_freq(input: &[u8], frame: usize) -> Map {
-    let mut freq = Map::default();
-    for code in Iter::new(input, frame) {
-        *freq.entry(code).or_insert(0) += 1;
-    }
-    freq
-}
-
-#[derive(Clone, Copy)]
-struct Freq(usize);
-#[derive(Clone, Copy)]
-struct Occ(&'static str);
-
-impl Freq {
-    fn print(&self, freq: &Map) {
-        let mut v: Vec<_> = freq.iter().map(|(&code, &count)| (count, code)).collect();
-        v.sort();
-        let total = v.iter().map(|&(count, _)| count).sum::<u32>() as f32;
-        for &(count, key) in v.iter().rev() {
-            println!(
-                "{} {:.3}",
-                key.to_string(self.0),
-                (count as f32 * 100.) / total
-            );
-        }
-        println!("");
-    }
-}
-impl Occ {
-    fn print(&self, freq: &Map) {
-        let count = if freq.contains_key(&Code::from_str(self.0)) {
-            freq[&Code::from_str(self.0)]
-        } else {
-            0
-        };
-        println!("{}\t{}", count, self.0);
-    }
-}
-
-fn read_input() -> Vec<u8> {
-    let file_name = std::env::args_os()
-        .nth(1)
-        .and_then(|s| s.into_string().ok())
-        .unwrap_or("25000_in".into());
-
-    let file = std::fs::File::open(file_name).unwrap();
-    let mut r = BufReader::new(file);
-    let key = ">THREE";
-    let mut res = Vec::with_capacity(65536);
-    let mut line = String::with_capacity(64);
-
-    loop {
-        match r.read_line(&mut line) {
-            Ok(b) if b > 0 => {
-                if line.starts_with(key) {
-                    break;
-                }
-            }
-            _ => break,
-        }
-        line.clear();
-    }
-
-    loop {
-        line.clear();
-        match r.read_line(&mut line) {
-            Ok(b) if b > 0 => {
-                let bytes = line.as_bytes();
-                res.extend(bytes[..bytes.len() - 1].into_iter().map(Code::encode_byte))
-            }
-            _ => break,
-        }
-    }
-    res
+fn inner(depth: i32, iterations: i32) -> String {
+    let chk: i32 = (0 .. iterations).into_par_iter().map(|_| {
+        let arena = Arena::new();
+        let a = bottom_up_tree(&arena, depth);
+        item_check(a)
+    }).sum();
+    format!("{}\t trees of depth {}\t check: {}", iterations, depth, chk)
 }
 
 fn main() {
-    let occs = vec![
-        Occ("GGTATTTTAATTTATAGT"),
-        Occ("GGTATTTTAATT"),
-        Occ("GGTATT"),
-        Occ("GGTA"),
-        Occ("GGT"),
-    ];
-    let input = Arc::new(read_input());
+    let n = std::env::args().nth(1)
+        .and_then(|n| n.parse().ok())
+        .unwrap_or(10);
+    let min_depth = 4;
+    let max_depth = if min_depth + 2 > n { min_depth + 2 } else { n };
 
-    // In reverse to spawn big tasks first
-    let results: Vec<_> = occs
-        .into_iter()
-        .map(|item| {
-            let input = input.clone();
-            thread::spawn(move || (item, gen_freq(&input, item.0.len())))
-        })
-        .collect();
-
-    Freq(1).print(&gen_freq(&input, 1));
-    Freq(2).print(&gen_freq(&input, 2));
-
-    for t in results.into_iter().rev() {
-        let (item, freq) = t.join().unwrap();
-        item.print(&freq);
+    {
+        let arena = Arena::new();
+        let depth = max_depth + 1;
+        let tree = bottom_up_tree(&arena, depth);
+        println!("stretch tree of depth {}\t check: {}", depth, item_check(tree));
     }
+
+    let long_lived_arena = Arena::new();
+    let long_lived_tree = bottom_up_tree(&long_lived_arena, max_depth);
+
+    let messages = (min_depth/2..max_depth/2 + 1).into_par_iter().map(|half_depth| {
+            let depth = half_depth * 2;
+            let iterations = 1 << ((max_depth - depth + min_depth) as u32);
+            inner(depth, iterations)
+        }).collect::<Vec<_>>();
+
+    for message in messages {
+        println!("{}", message);
+    }
+
+    println!("long lived tree of depth {}\t check: {}", max_depth, item_check(long_lived_tree));
 }
