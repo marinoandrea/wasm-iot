a596ac2e1d5cc5ff747bb6c9cfaafeca9579beb8
Andrea Marino <am.marinoandrea@gmail.com>
2022-11-03 18:50:52 +0100

    Added export PDF logic to R histograms

--- analysis/visualization/histograms.r
+++ analysis/visualization/histograms.r
@@ -6,25 +6,33 @@ source("utils.r")
 base_theme    <- theme(text = element_text(size = 20))
 base_style    <- geom_histogram(bins=60, color="black", fill="lightblue")
 
-plot_histograms <- function(df_partial) {
+plot_histograms <- function(df_partial, discriminator_name) {
+  pdf(file=sprintf("%s_energy_usage.pdf", discriminator_name))
   energy <- 
     ggplot(data=df_partial, aes(x=energy_usage)) + 
     base_style + 
     base_theme + 
     labs(y='samples', x='energy usage (Joules)')
+  print(energy)
+  dev.off()
+
+  pdf(file=sprintf("%s_execution_time.pdf", discriminator_name))
   time <- 
     ggplot(data=df_partial, aes(x=execution_time)) + 
     base_style + 
     base_theme + 
     labs(y='samples', x='execution time (s)')
+  print(time)
+  dev.off()
+
+  pdf(file=sprintf("%s_memory_usage.pdf", discriminator_name))
   memory <- 
     ggplot(data=df_partial, aes(x=memory_usage)) + 
     base_style + 
     base_theme + 
     labs(y='samples', x='memory usage (%)')
-  print(energy)
-  print(time)
   print(memory)
+  dev.off()
 }
 
 main <- function() {
@@ -33,20 +41,20 @@ main <- function() {
   # Hypothesis H^pl
   for (language in c('c', 'go', 'rust', 'javascript')) {
     df_partial <- subset(df, df$language == language)
-    plot_histograms(df_partial)
+    plot_histograms(df_partial, language)
   }
 
   # Hypothesis H^re
   for (runtime in c('wasmer', 'wasmtime')) {
     df_partial <- subset(df, df$runtime == runtime)
-    plot_histograms(df_partial)
+    plot_histograms(df_partial, runtime)
   }
 
   # Hypothesis H^{pl, re}
   for (language in c('c', 'go', 'rust', 'javascript')) {
     for (runtime in c('wasmer', 'wasmtime')) {
       df_partial <- subset(df, df$runtime == runtime & df$language == language)
-      plot_histograms(df_partial)
+      plot_histograms(df_partial, sprintf("%s_%s", language, runtime))
     }
   }
 }
