2b57cb461ea03c9287a8ad627104bd04e3c6ba60
Andrea Marino <am.marinoandrea@gmail.com>
2022-11-03 18:45:04 +0100

    Added export PDF logic to R boxplots

--- analysis/visualization/boxplots.r
+++ analysis/visualization/boxplots.r
@@ -7,15 +7,33 @@ source("utils.r")
 base_theme    <- theme(text = element_text(size = 20))
 base_outliars <- geom_boxplot(outlier.colour="red", outlier.size=1)
 
-plot_boxplot <- function(aes_selection, x_label, y_label, log) {
-  plot <- ggplot(df, aes(x=language, y=energy_usage, color=language)) +
+format_field_name <- function(field) {
+  return(substring(toString(field), first=4))
+}
+
+setup_pdf_storage <- function(aes_selection) {
+  x <- format_field_name(aes_selection$x)
+  y <- format_field_name(aes_selection$y)
+  if (exists('fill', where=aes_selection)) {
+    z <- format_field_name(aes_selection$fill)
+    pdf(file=sprintf("%s_%s_%s.pdf", x, z, y))
+  } else {
+    pdf(file=sprintf("%s_%s.pdf", x, y))
+  }
+}
+
+plot_boxplot <- function(df, aes_selection, x_label, y_label, log) {
+  setup_pdf_storage(aes_selection)
+  plot <- ggplot(df, aes_selection) +
     base_outliars +
     base_theme +
-    labs(x=x_label, y=y_label)
+    labs(x=x_label, y=y_label) +
+    scale_fill_grey()
   if (log) { 
     plot <- plot + scale_y_continuous(trans='log2')
   }
   print(plot)
+  dev.off()
 }
   
 main <- function() {
@@ -23,16 +41,19 @@ main <- function() {
 
   # Hypothesis H^pl
   plot_boxplot(
-    aes(x=language, y=energy_usage, color=language), 
+    df,
+    aes(x=language, y=energy_usage, color=language),
     x_label="programming language",
     y_label="energy usage (Joules)",
     log=TRUE)
   plot_boxplot(
+    df,
     aes(x=language, y=execution_time, color=language), 
     x_label="programming language",
     y_label="execution time (s)",
     log=TRUE)
   plot_boxplot(
+    df,
     aes(x=language, y=memory_usage, color=language), 
     x_label="programming language",
     y_label="memory usage (%)",
@@ -40,16 +61,19 @@ main <- function() {
 
   # Hypothesis H^re
   plot_boxplot(
+    df,
     aes(x=runtime, y=energy_usage, color=runtime), 
     x_label="runtime",
     y_label="energy usage (Joules)",
     log=TRUE)
   plot_boxplot(
+    df,
     aes(x=runtime, y=execution_time, color=runtime), 
     x_label="runtime",
     y_label="execution time (s)",
     log=TRUE)
   plot_boxplot(
+    df,
     aes(x=runtime, y=memory_usage, color=runtime), 
     x_label="runtime",
     y_label="memory usage (%)",
@@ -57,16 +81,19 @@ main <- function() {
 
   # Hypothesis H^{pl, re}
   plot_boxplot(
+    df,
     aes(x=language, y=energy_usage, color=language, fill=runtime),
     x_label="programming language",
     y_label="energy usage (Joules)",
     log=TRUE)
   plot_boxplot(
+    df,
     aes(x=language, y=execution_time, color=language, fill=runtime),
     x_label="programming language",
     y_label="execution time (s)",
     log=TRUE)
   plot_boxplot(
+    df,
     aes(x=language, y=memory_usage, color=language, fill=runtime),
     x_label="programming language",
     y_label="memory usage (%)",
