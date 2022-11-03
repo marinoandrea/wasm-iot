678941d989b01e80238f1e8d38bf162bc5faff08
Andrea Marino <am.marinoandrea@gmail.com>
2022-11-03 14:31:00 +0100

    Updated histograms visualization for hypothesis 3

--- analysis/visualization/histograms.r
+++ analysis/visualization/histograms.r
@@ -12,10 +12,7 @@ df <- data.frame(
 base_theme    <- theme(text = element_text(size = 20))
 base_style    <- geom_histogram(bins=60, color="black", fill="lightblue")
 
-# Hypothesis H^pl
-
-for (language in c('c', 'go', 'rust', 'javascript')) {
-  df_partial <- subset(df, df$language == language)
+plot_histograms <- function(df_partial) {
   energy <- 
     ggplot(data=df_partial, aes(x=energy_usage)) + 
     base_style + 
@@ -36,26 +33,24 @@ for (language in c('c', 'go', 'rust', 'javascript')) {
   print(memory)
 }
 
+# Hypothesis H^pl
+
+for (language in c('c', 'go', 'rust', 'javascript')) {
+  df_partial <- subset(df, df$language == language)
+  plot_histograms(df_partial)
+}
+
 # Hypothesis H^re
 
 for (runtime in c('wasmer', 'wasmtime')) {
   df_partial <- subset(df, df$runtime == runtime)
-  energy <- 
-    ggplot(data=df_partial, aes(x=energy_usage)) + 
-    base_style + 
-    base_theme + 
-    labs(y='samples', x='energy usage (Joules)')
-  time <- 
-    ggplot(data=df_partial, aes(x=execution_time)) + 
-    base_style + 
-    base_theme + 
-    labs(y='samples', x='execution time (s)')
-  memory <- 
-    ggplot(data=df_partial, aes(x=memory_usage)) + 
-    base_style + 
-    base_theme + 
-    labs(y='samples', x='memory usage (%)')
-  print(energy)
-  print(time)
-  print(memory)
+  plot_histograms(df_partial)
+}
+
+# Hypothesis H^{pl, re}
+for (language in c('c', 'go', 'rust', 'javascript')) {
+  for (runtime in c('wasmer', 'wasmtime')) {
+    df_partial <- subset(df, df$runtime == runtime & df$language == language)
+    plot_histograms(df_partial)
+  }
 }
