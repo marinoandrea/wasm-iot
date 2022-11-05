1db30153a7bfb3972a0aa376b8bf06a31dfb870a
Andrea Marino <am.marinoandrea@gmail.com>
2022-11-05 17:03:58 +0100

    Updated R `analysis.visualization` imports

--- analysis/visualization/boxplots.r
+++ analysis/visualization/boxplots.r
@@ -1,24 +1,24 @@
-if (!require("ggplot2")) install.packages("ggplot2"); 
+if (!require("ggplot2")) install.packages("ggplot2")
 
 library(ggplot2)
-source("utils.r")
+source("../core/utils.r")
 
 # base visualization style settings
-base_theme    <- theme(text = element_text(size = 20))
-base_outliars <- geom_boxplot(outlier.colour="red", outlier.size=1)
+base_theme <- theme(text = element_text(size = 20))
+base_outliars <- geom_boxplot(outlier.colour = "red", outlier.size = 1)
 
 format_field_name <- function(field) {
-  return(substring(toString(field), first=4))
+  return(substring(toString(field), first = 4))
 }
 
 setup_pdf_storage <- function(aes_selection) {
   x <- format_field_name(aes_selection$x)
   y <- format_field_name(aes_selection$y)
-  if (exists('fill', where=aes_selection)) {
+  if (exists("fill", where = aes_selection)) {
     z <- format_field_name(aes_selection$fill)
-    pdf(file=sprintf("%s_%s_%s.pdf", x, z, y))
+    pdf(file = sprintf("%s_%s_%s.pdf", x, z, y))
   } else {
-    pdf(file=sprintf("%s_%s.pdf", x, y))
+    pdf(file = sprintf("%s_%s.pdf", x, y))
   }
 }
 
@@ -27,79 +27,88 @@ plot_boxplot <- function(df, aes_selection, x_label, y_label, log) {
   plot <- ggplot(df, aes_selection) +
     base_outliars +
     base_theme +
-    labs(x=x_label, y=y_label) +
+    labs(x = x_label, y = y_label) +
     scale_fill_grey()
-  if (log) { 
-    plot <- plot + scale_y_continuous(trans='log2')
+  if (log) {
+    plot <- plot + scale_y_continuous(trans = "log2")
   }
   print(plot)
   dev.off()
 }
-  
+
 main <- function() {
   df <- read_dataset()
 
   # Hypothesis H^pl
   plot_boxplot(
     df,
-    aes(x=language, y=energy_usage, color=language),
-    x_label="programming language",
-    y_label="energy usage (Joules)",
-    log=TRUE)
+    aes(x = language, y = energy_usage, color = language),
+    x_label = "programming language",
+    y_label = "energy usage (Joules)",
+    log = TRUE
+  )
   plot_boxplot(
     df,
-    aes(x=language, y=execution_time, color=language), 
-    x_label="programming language",
-    y_label="execution time (s)",
-    log=TRUE)
+    aes(x = language, y = execution_time, color = language),
+    x_label = "programming language",
+    y_label = "execution time (s)",
+    log = TRUE
+  )
   plot_boxplot(
     df,
-    aes(x=language, y=memory_usage, color=language), 
-    x_label="programming language",
-    y_label="memory usage (%)",
-    log=TRUE)
+    aes(x = language, y = memory_usage, color = language),
+    x_label = "programming language",
+    y_label = "memory usage (%)",
+    log = TRUE
+  )
 
   # Hypothesis H^re
   plot_boxplot(
     df,
-    aes(x=runtime, y=energy_usage, color=runtime), 
-    x_label="runtime",
-    y_label="energy usage (Joules)",
-    log=TRUE)
+    aes(x = runtime, y = energy_usage, color = runtime),
+    x_label = "runtime",
+    y_label = "energy usage (Joules)",
+    log = TRUE
+  )
   plot_boxplot(
     df,
-    aes(x=runtime, y=execution_time, color=runtime), 
-    x_label="runtime",
-    y_label="execution time (s)",
-    log=TRUE)
+    aes(x = runtime, y = execution_time, color = runtime),
+    x_label = "runtime",
+    y_label = "execution time (s)",
+    log = TRUE
+  )
   plot_boxplot(
     df,
-    aes(x=runtime, y=memory_usage, color=runtime), 
-    x_label="runtime",
-    y_label="memory usage (%)",
-    log=TRUE)
+    aes(x = runtime, y = memory_usage, color = runtime),
+    x_label = "runtime",
+    y_label = "memory usage (%)",
+    log = TRUE
+  )
 
   # Hypothesis H^{pl, re}
   plot_boxplot(
     df,
-    aes(x=language, y=energy_usage, color=language, fill=runtime),
-    x_label="programming language",
-    y_label="energy usage (Joules)",
-    log=TRUE)
+    aes(x = language, y = energy_usage, color = language, fill = runtime),
+    x_label = "programming language",
+    y_label = "energy usage (Joules)",
+    log = TRUE
+  )
   plot_boxplot(
     df,
-    aes(x=language, y=execution_time, color=language, fill=runtime),
-    x_label="programming language",
-    y_label="execution time (s)",
-    log=TRUE)
+    aes(x = language, y = execution_time, color = language, fill = runtime),
+    x_label = "programming language",
+    y_label = "execution time (s)",
+    log = TRUE
+  )
   plot_boxplot(
     df,
-    aes(x=language, y=memory_usage, color=language, fill=runtime),
-    x_label="programming language",
-    y_label="memory usage (%)",
-    log=TRUE)
+    aes(x = language, y = memory_usage, color = language, fill = runtime),
+    x_label = "programming language",
+    y_label = "memory usage (%)",
+    log = TRUE
+  )
 }
 
-if(!interactive()) {
+if (!interactive()) {
   main()
 }
--- analysis/visualization/histograms.r
+++ analysis/visualization/histograms.r
@@ -1,36 +1,36 @@
-if (!require("ggplot2")) install.packages("ggplot2");
+if (!require("ggplot2")) install.packages("ggplot2")
 
 library(ggplot2)
-source("utils.r")
+source("../core/utils.r")
 
-base_theme    <- theme(text = element_text(size = 20))
-base_style    <- geom_histogram(bins=60, color="black", fill="lightblue")
+base_theme <- theme(text = element_text(size = 20))
+base_style <- geom_histogram(bins = 60, color = "black", fill = "lightblue")
 
 plot_histograms <- function(df_partial, discriminator_name) {
-  pdf(file=sprintf("%s_energy_usage.pdf", discriminator_name))
-  energy <- 
-    ggplot(data=df_partial, aes(x=energy_usage)) + 
-    base_style + 
-    base_theme + 
-    labs(y='samples', x='energy usage (Joules)')
+  pdf(file = sprintf("%s_energy_usage.pdf", discriminator_name))
+  energy <-
+    ggplot(data = df_partial, aes(x = energy_usage)) +
+    base_style +
+    base_theme +
+    labs(y = "samples", x = "energy usage (Joules)")
   print(energy)
   dev.off()
 
-  pdf(file=sprintf("%s_execution_time.pdf", discriminator_name))
-  time <- 
-    ggplot(data=df_partial, aes(x=execution_time)) + 
-    base_style + 
-    base_theme + 
-    labs(y='samples', x='execution time (s)')
+  pdf(file = sprintf("%s_execution_time.pdf", discriminator_name))
+  time <-
+    ggplot(data = df_partial, aes(x = execution_time)) +
+    base_style +
+    base_theme +
+    labs(y = "samples", x = "execution time (s)")
   print(time)
   dev.off()
 
-  pdf(file=sprintf("%s_memory_usage.pdf", discriminator_name))
-  memory <- 
-    ggplot(data=df_partial, aes(x=memory_usage)) + 
-    base_style + 
-    base_theme + 
-    labs(y='samples', x='memory usage (%)')
+  pdf(file = sprintf("%s_memory_usage.pdf", discriminator_name))
+  memory <-
+    ggplot(data = df_partial, aes(x = memory_usage)) +
+    base_style +
+    base_theme +
+    labs(y = "samples", x = "memory usage (%)")
   print(memory)
   dev.off()
 }
@@ -39,27 +39,26 @@ main <- function() {
   df <- read_dataset()
 
   # Hypothesis H^pl
-  for (language in c('c', 'go', 'rust', 'javascript')) {
+  for (language in c("c", "go", "rust", "javascript")) {
     df_partial <- subset(df, df$language == language)
     plot_histograms(df_partial, language)
   }
 
   # Hypothesis H^re
-  for (runtime in c('wasmer', 'wasmtime')) {
+  for (runtime in c("wasmer", "wasmtime")) {
     df_partial <- subset(df, df$runtime == runtime)
     plot_histograms(df_partial, runtime)
   }
 
   # Hypothesis H^{pl, re}
-  for (language in c('c', 'go', 'rust', 'javascript')) {
-    for (runtime in c('wasmer', 'wasmtime')) {
+  for (language in c("c", "go", "rust", "javascript")) {
+    for (runtime in c("wasmer", "wasmtime")) {
       df_partial <- subset(df, df$runtime == runtime & df$language == language)
       plot_histograms(df_partial, sprintf("%s_%s", language, runtime))
     }
   }
 }
 
-if(!interactive()) {
+if (!interactive()) {
   main()
 }
-
